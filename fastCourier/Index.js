ee0af992f1208e6a4a14171fbf9719ae387004f2
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-10-20 18:46:31 +0600

    view from html

---
+++ fastCourier/Index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    // res.send('<p>Welcome to node JS</p>');
+    res.sendFile(path.join(__dirname, 'public','index.html'));
+});
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server http://localhost:${PORT}`);
+});
